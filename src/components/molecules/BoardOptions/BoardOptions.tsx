988ebe5c4a269001c8501cdf2768530fd9de4eb2
EdiedRamos <edieisa22@gmail.com>
2024-04-12 22:16:40 -0500

    feat: add turn mark icon

--- src/components/molecules/BoardOptions/BoardOptions.tsx
+++ src/components/molecules/BoardOptions/BoardOptions.tsx
@@ -1,18 +1,26 @@
 import { Logo, SmallButton, TurnLabel } from "@/components/atoms";
-
 import { Icons } from "@/general/icons";
+import { useGame } from "@/hooks";
 
 import "./BoardOptions.scss";
 
 const { LetterO, LetterX } = Icons;
 
 export const BoardOptions = () => {
+  const { currentMark } = useGame();
+
   return (
     <div className="board-options__container">
       <Logo />
       <TurnLabel>
-        {<LetterO className="icon" style={{ fontSize: "1.5rem" }} />}{" "}
-        {<LetterX className="icon" style={{ fontSize: "1.5rem" }} />} TURN
+        {currentMark === "x" ? (
+          <LetterX className="icon" style={{ fontSize: "1.5rem" }} />
+        ) : (
+          <LetterO className="icon" style={{ fontSize: "1.5rem" }} />
+        )}
+        <span style={{ marginLeft: "5px", background: "transparent" }}>
+          TURN
+        </span>
       </TurnLabel>
       <SmallButton disabled={false} onClick={() => {}}>
         <Icons.Reload className="icon" style={{ fontSize: "1.5rem" }} />
