e1120d4da3370cb5c197dcafb0702da4d4b971b7
EdiedRamos <edieisa22@gmail.com>
2024-04-17 22:40:15 -0500

    style: update modal styles

--- src/components/molecules/modal/Modal.tsx
+++ src/components/molecules/modal/Modal.tsx
@@ -5,17 +5,12 @@ import { useUI } from "@/hooks";
 import "./Modal.scss";
 
 export const Modal = () => {
-  const { showModal, closeModal, body } = useUI();
+  const { showModal, body } = useUI();
 
   if (!showModal) return null;
   return ReactDom.createPortal(
     <div className="modal-overlay">
-      <div className="modal">
-        <button className="modal-close" onClick={closeModal}>
-          Cerrar
-        </button>
-        {body}
-      </div>
+      <div className="modal">{body}</div>
     </div>,
     document.getElementById("modal-root")!
   );
