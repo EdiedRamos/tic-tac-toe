867d1fccccb9f5eb8d6a1c9a5214da960e1b501b
EdiedRamos <edieisa22@gmail.com>
2024-04-18 03:09:20 -0500

    fix: remove wrong file

--- src/components/molecules/gameMode/PageManager.tsx
+++
@@ -1,7 +0,0 @@
-import { ternary } from "@/hooks";
-import { Game, Menu } from "@/pages";
-
-export const PageManager = () => {
-  const { screen } = ternary();
-  return <>{screen === "menu" ? <Menu /> : <Game />}</>;
-};
